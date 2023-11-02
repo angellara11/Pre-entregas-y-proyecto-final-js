e1debdf7eeadb3dbc4f2f02bc059632ccf749098
angellara11 <139193019+angellara11@users.noreply.github.com>
2023-11-01 21:30:00 -0300

    PreEntrega1Lara

--- PreEntrega1Lara/main.js
+++ PreEntrega1Lara/main.js
@@ -1,79 +1,69 @@
-// // Declaración de variables
-// let nombre = prompt("Como te llamas?");
-// let edad = prompt("Que edad tienes?");
-
-// // Función que saluda
-// function saludar(nombre) {
-//   alert("¡Hola, " + nombre + "!");
-// }
-
-// // Función que determina si una persona es mayor de edad
-// function esMayorDeEdad(edad) {
-//   if (edad >= 18) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-
-// // Control de flujos
-// if (esMayorDeEdad(edad)) {
-//   saludar(nombre);
-//   alert("Eres mayor de edad.");
-// } else {
-//   saludar(nombre);
-//   alert("Eres menor de edad.");
-// }
-
-// // Ciclo - Bucle for
-// for (let i = 1; i <= 5; i++) {
-//   alert("Iteración #" + i);
-// }
-
-// // Interacciones con el usuario
-// let numero = prompt("Ingresa un número:");
-// numero = parseInt(numero);
-
-// if (!isNaN(numero)) {
-//   alert("El doble de " + numero + " es " + (numero * 2));
-// } else {
-//   alert("No ingresaste un número válido.");
-// }
-
 alert(
   "Hola, antes de comenzar necesito saber tu edad debido a la politica de uso responsable de bebidas alcoholicas, NO PODEMOS VENDER ALCOHOL A MENORES DE 18 AÑOS."
 );
 
-for (;;) {
-  let edad = prompt("Que edad tienes?");
-  if (edad >= 18) {
-    alert(
-      "Eres mayor de edad, puedes beber alcohol, recuerda beber con precaucion."
-    );
-    break;
-  } else if (edad === "") {
-    alert("Por favor, ingresa una edad");
-  } else {
-    alert(
-      "Eres menor de edad, no podemos venderte alcohol, esperamos que nos entiendas."
-    );
-  }
-}
-
-let nombre = prompt("Como te llamas?").toUpperCase();
-let preferenciaCliente = prompt(
-  `Buenas ${nombre}, que desearias tomar, tenemos margarita, mojito y daquiri.`
-);
-
-if (preferenciaCliente === "margarita" || preferenciaCliente === "Margarita") {
-  alert("Marchando un Margarita para " + nombre + ".");
-} else if (preferenciaCliente === "mojito" || preferenciaCliente === "Mojito") {
-  alert("Marchando un Mojito para " + nombre + ".");
-} else if (preferenciaCliente === "daquiri") {
-  alert(`Marchando un Daquiri para ${nombre}.`);
+let edad = prompt("Que edad tienes?");
+if (edad >= 18) {
+  alert(
+    "Eres mayor de edad, puedes beber alcohol, recuerda beber con precaucion."
+  );
+  let nombre = prompt("Como te llamas?").toUpperCase();
+  pedirBebida(nombre);
 } else {
-  alert("No tenemos esa bebida en nuestro menú.");
+  alert(
+    "Eres menor de edad, no podemos venderte alcohol, esperamos que nos entiendas."
+  );
 }
+
+function pedirBebida(nombre) {
+  pidiendo = true;
+  let totalCuenta = 0;
+  do {
+    let preferenciaCliente = prompt(
+      `Buenas ${nombre}, que desearias tomar, tenemos: \n\nmargarita 12£ \nmojito 10£ \ndaquiri 18£\n\n dime "pagar" cuando quieras abonar tu cuenta\n\n`
+    );
+    // // use la concatenacion y la interpolacion del codigo para mostras varias formas de hacerlo ademas hice uso del or || como ejemplo de lo que aprendi
+    if (
+      preferenciaCliente === "margarita" ||
+      preferenciaCliente === "Margarita"
+    ) {
+      alert("Marchando un Margarita para " + nombre + ".");
+      totalCuenta = totalCuenta + 12;
+    } else if (
+      preferenciaCliente === "mojito" ||
+      preferenciaCliente === "Mojito"
+    ) {
+      alert("Marchando un Mojito para " + nombre + ".");
+      totalCuenta = totalCuenta + 10;
+    } else if (preferenciaCliente === "daquiri") {
+      alert(`Marchando un Daquiri para ${nombre}.`);
+      totalCuenta = totalCuenta + 18;
+    } else if (preferenciaCliente === "pagar") {
+      alert(`el total de su cuenta es ${totalCuenta}`);
+      pidiendo = false;
+    } else {
+      alert("No tenemos esa bebida en nuestro menú.");
+    }
+  } while (pidiendo);
+}
+
+// for (;;) {
+//   let edad = prompt("Que edad tienes?");
+//   if (edad >= 18) {
+//     alert(
+//       "Eres mayor de edad, puedes beber alcohol, recuerda beber con precaucion."
+//     );
+//     break;
+//   } else if (edad === "") {
+//     alert("Por favor, ingresa una edad");
+//   } else {
+//   }
+// }
+
+// let nombre = prompt("Como te llamas?").toUpperCase();
+// let preferenciaCliente = prompt(
+//   `Buenas ${nombre}, que desearias tomar, tenemos margarita, mojito y daquiri.`
+// );
 
 // function loguear(){
 
