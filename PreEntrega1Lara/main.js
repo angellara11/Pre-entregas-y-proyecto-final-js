5f7974e8ecfe2038a5cc2a89cc514d61db9acec5
angellara11 <139193019+angellara11@users.noreply.github.com>
2023-11-01 21:40:31 -0300

    PreEntrega1Lara

--- PreEntrega1Lara/main.js
+++ PreEntrega1Lara/main.js
@@ -39,58 +39,10 @@ function pedirBebida(nombre) {
       alert(`Marchando un Daquiri para ${nombre}.`);
       totalCuenta = totalCuenta + 18;
     } else if (preferenciaCliente === "pagar") {
-      alert(`el total de su cuenta es ${totalCuenta}`);
+      alert(`el total de su cuenta es ${totalCuenta}£`);
       pidiendo = false;
     } else {
       alert("No tenemos esa bebida en nuestro menú.");
     }
   } while (pidiendo);
 }
-
-// for (;;) {
-//   let edad = prompt("Que edad tienes?");
-//   if (edad >= 18) {
-//     alert(
-//       "Eres mayor de edad, puedes beber alcohol, recuerda beber con precaucion."
-//     );
-//     break;
-//   } else if (edad === "") {
-//     alert("Por favor, ingresa una edad");
-//   } else {
-//   }
-// }
-
-// let nombre = prompt("Como te llamas?").toUpperCase();
-// let preferenciaCliente = prompt(
-//   `Buenas ${nombre}, que desearias tomar, tenemos margarita, mojito y daquiri.`
-// );
-
-// function loguear(){
-
-//   let identificar = true
-//   let intentos = 1
-
-//   do{
-//       let usuario = prompt("ingresa tu usuario (solo 3 intentos)")
-//       if(usuario === null){ //si usser es null se salta al siguiente paso
-//           break
-//       }
-
-//       if(usuario== "javier gimenez"  && intentos <=3){
-//           alert("bienvenido usuario " + usuario)
-//           identificar = false
-//       }else{
-//           alert("no se reconoce el usuario " + usuario)
-//           intentos++
-//           if(intentos >3){
-//               alert("usted supero los 3 intentos, vuelva mas tarde")
-//               console.error("no se reconoce el usuario")
-//               break
-//           }
-//       }
-
-//   }while(identificar)
-
-// }
-
-// loguear()
